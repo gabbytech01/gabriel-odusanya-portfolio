c7dbfb0a3672e85d2f3b4eb4b572b5c2053003ba
Gabriel Odusanya <gabbycloudsec@gmail.com>
2026-08-11 17:56:22 +0100

    Add GitHub Pages deployment workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  output: "export",
+
   images: {
-    remotePatterns: [
-      { protocol: "https", hostname: "**" }
-    ]
-  }
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
